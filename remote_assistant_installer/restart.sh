64396bff7e42dfca3ae63b53306aadba63793200
Thorsten Heilmann <info@looking4cache.com>
2025-04-18 19:36:03 +0200

    config with jq

--- remote_assistant_installer/restart.sh
+++ remote_assistant_installer/restart.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ "${ENABLE_RESTART_OPTIONS}" = "true" ]; then
-    echo "🔄 Restarting Home Assistant Core..."
-    curl -s -X POST \
-        -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
-        -H "Content-Type: application/json" \
-        http://supervisor/core/restartelse
+CONFIG_FILE="/data/options.json"
+if [[ -f "$CONFIG_FILE" ]]; then
+    ENABLE_RESTART=$(jq -r '.enable_restart // false' "$CONFIG_FILE")
+    if [[ "$ENABLE_RESTART" = "true" ]]; then
+        echo "🔄 Restarting Home Assistant Core..."
+        curl -s -X POST \
+            -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
+            -H "Content-Type: application/json" \
+            http://supervisor/core/restartelse
+    else
+        echo "🔄 Please restart Home Assistant for the changes to take effect."
+    fi
 else
     echo "🔄 Please restart Home Assistant for the changes to take effect."
 fi
